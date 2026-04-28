b5e3592008c19f99e8a9deda676a7b24b36ecaaa
Huỳnh Trọng Nhân <142094267+NhanItJAVA1@users.noreply.github.com>
2026-04-28 21:43:29 +0700

    Update

--- movie-ticket-system/services/notification-service/src/notification/notification.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { NotificationService } from "./notification.service";
-import { KafkaConsumer } from "../kafka/kafka.consumer";
-
-@Module({
-  providers: [NotificationService, KafkaConsumer],
-})
-export class NotificationModule {}
